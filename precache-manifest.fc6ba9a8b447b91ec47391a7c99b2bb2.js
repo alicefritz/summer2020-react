dc114d23f6658881517a5ad4b4f93bcba9538f81
alicefritz <44866891+alicefritz@users.noreply.github.com>
2020-02-08 23:01:39 +0100

    Updates

---
+++ precache-manifest.fc6ba9a8b447b91ec47391a7c99b2bb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb73bf36b29725967dfbb11ec2a9fa3",
+    "url": "/summer2020-react/index.html"
+  },
+  {
+    "revision": "02d65f066887ee7a59a4",
+    "url": "/summer2020-react/static/css/main.0af2d106.chunk.css"
+  },
+  {
+    "revision": "7ee37bcb574d8f5fe9fe",
+    "url": "/summer2020-react/static/js/2.34042c04.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/summer2020-react/static/js/2.34042c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d65f066887ee7a59a4",
+    "url": "/summer2020-react/static/js/main.c2f51b39.chunk.js"
+  },
+  {
+    "revision": "3cb182dcf45d3fc65bc0",
+    "url": "/summer2020-react/static/js/runtime-main.5a638911.js"
+  }
+]);
